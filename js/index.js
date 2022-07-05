6a532e2d163ebd7ea4b2ee0440c65a6d40ad6256
quac17 <quac1701@gmail.com>
2022-07-06 01:59:01 +0700

    weather-App

---
+++ js/index.js
@@ -0,0 +1,39 @@
+
+
+const APP_ID = 'c56c89340d5aee5bf8eebffd308436f6';
+const DEFAULT_VALUE = "";
+const searchInput = document.querySelector('#search-input');
+const cityName = document.querySelector('.city-name');
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherState = document.querySelector('.weather-state');
+const temperature = document.querySelector('.temperature');
+
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind-speed');
+
+searchInput.addEventListener('change', (e)=>{
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${e.target.value}&appid=${APP_ID}`)
+    .then (async res =>{
+        const geo_data = await res.json();
+        console.log('Geo_data', geo_data);
+        // console.log("toa do",geo_data[0].lat," ",geo_data[0].lon);
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo_data[0].lat}&lon=${geo_data[0].lon}&units=metric&lang=vi&appid=${APP_ID}`)
+        .then (async res=>{
+            const data = await res.json();
+            console.log('weather_data', data);
+            // console.log(data.weather[0].icon);
+            cityName.innerHTML = data.name || DEFAULT_VALUE;
+            weatherIcon.setAttribute('src',`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+            temperature.innerHTML = (data.main.temp).toFixed(1) || DEFAULT_VALUE;
+            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+
+            // console.log(data.main.humidity);
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;            humidity.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+            windSpeed.innerHTML = (data.wind.speed*3.6).toFixed(2) || DEFAULT_VALUE;
+            humidity.innerHTML = data.main.humidity|| DEFAULT_VALUE;
+        });
+    });
+});
---
+++ readme.txt
@@ -0,0 +1,4 @@
+APP sử dụng API để hiển thị thông tin về thời tiết tại những địa điểm nhất định
+app sử dụng 2 API:
+- Api trả về tọa độ địa lý của địa điểm được người dùng đánh lên thanh tìm kiếm.
+- Api dùng tọa độ của địa điểm để trả về các chỉ số thời tiết realtime
