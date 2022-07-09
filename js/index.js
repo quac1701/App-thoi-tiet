1b32f068586e2e1cdf3ea7980a5a4acc0bc1fdc9
quac17 <quac1701@gmail.com>
2022-07-10 00:38:01 +0700

    done

--- js/index.js
+++ js/index.js
@@ -18,12 +18,12 @@ searchInput.addEventListener('change', (e)=>{
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${e.target.value}&appid=${APP_ID}`)
     .then (async res =>{
         const geo_data = await res.json();
-        console.log('Geo_data', geo_data);
+        // console.log('Geo_data', geo_data);
         // console.log("toa do",geo_data[0].lat," ",geo_data[0].lon);
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo_data[0].lat}&lon=${geo_data[0].lon}&units=metric&lang=vi&appid=${APP_ID}`)
         .then (async res=>{
             const data = await res.json();
-            console.log('weather_data', data);
+            // console.log('weather_data', data);
             // console.log(data.weather[0].icon);
             cityName.innerHTML = data.name || DEFAULT_VALUE;
             weatherIcon.setAttribute('src',`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
@@ -50,11 +50,11 @@ recognition.continuous = false;
 const microphone = document.querySelector(' .microphone');
 
 const handleVoice = (text) => {
-    console.log('text',text); 
+    // console.log('text',text); 
     const handleText = text.toLowerCase();
     if (handleText.includes('thời tiết')){
         const location = handleText.split('tiết')[1].trim();
-        console.log(location);
+        // console.log(location);
         searchInput.value = location;
         const changeEvent = new Event('change');
         searchInput.dispatchEvent(changeEvent); 
@@ -75,7 +75,7 @@ recognition.onerror = (err) =>{
 }
 
 recognition.onresult = (e) =>{
-    console.log ('onresult',e);
+    // console.log ('onresult',e);
     const text = e.results[0][0].transcript;
     handleVoice(text);
 }
